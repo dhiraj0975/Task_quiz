f2495869799b56d36cd86ccabc34be3d7d39559b
Dhiraj Khobragade <dhirajkhobragade64@gmail.com>
2026-09-01 19:48:22 +0530

    Update data-source.js

--- backend/src/config/data-source.js
+++ backend/src/config/data-source.js
@@ -3,7 +3,21 @@ const { DataSource } = require('typeorm');
 const { Question } = require('../entities/Question');
 
 const AppDataSource = new DataSource({
+
+
+
+
+
+
+    
     type: "mysql",
+
+
+
+
+
+
+    
     host: process.env.DB_HOST || "localhost",
     port: process.env.DB_PORT || 3306,
     username: process.env.DB_USER || "root",
